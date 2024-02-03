4e5bca7aecef2eb7d22ffaf5a24c859d562f0c8a
kousaralya <kousaralya@gmail.com>
2024-02-03 01:03:45 -0500

    Initial commit

---
+++ exercise_5/fortune.js
@@ -0,0 +1,8 @@
+var numberOfChildren = (1,2);
+var person { partnerName = 'sita'}
+var location = 'Chennai';
+var jobtitle = 'developer';
+function tellFortune(numberOfChildren, partnerName, location, jobTitle) 
+    
+
+console.log(tellFortune);
